7314dc77b95001070e74a37d349853b8460522f0
Junmo1225 <junmo20091225@hotmail.com>
2021-09-29 19:10:53 +0700

    Update Junmo1(github).py

--- Junmo1(github).py
+++ Junmo1(github).py
@@ -1,7 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 29 19:00:11 2021
+company = '삼성전자'
+print(company)
+sid = '100'
+sec_url = 'https://news.never.com/main.list.nhn?mode=LSD&amp;mid=sec'+'&amp;sid1='+sid
+print('section url : ', sec_url)
 
-@author: Junmo
-"""
 
+pmhigh_foods = '콩나물밥, 고등어, 굴, 쌀국수, 마라탕'
+pmhigh_foods = pmhigh_foods.split(',')
+print('pmhigh_foods : ', pmhigh_foods)
+print('type(pmhigh_foods) : ', type(pmhigh_foods))
+sid_list = ['100', '101', '102']
+print('sid_list : ', sid_list)
+print('type(sid_list) : ', type(sid_list))
+print('sid_list[0] : ', sid_list[0])
+print('sid_list[1] : ', sid_list[1])
+print('sid_list[2] : ', sid_list[2])
